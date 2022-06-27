73dcb22da272ca5fb47dc437e8ddcc89a6db160c
Shirley Zhu <shirleyzzhu@fb.com>
2022-06-27 09:27:52 -0700

    added login screen and generating user feature

---
+++ Parse Chat/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  Parse Chat
+//
+//  Created by Shirley Zhu on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Parse Chat/LoginViewController.m
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.m
+//  Parse Chat
+//
+//  Created by Shirley Zhu on 6/27/22.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)registerUser:(id)sender {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    // set user properties
+    newUser.username = self.usernameTextField.text;
+    newUser.password = self.passwordTextField.text;
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
+}
+
+- (IBAction)loginUser:(id)sender {
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
